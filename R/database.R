d3f3a245b03968f7379f8a69f54610f389b9da98
Cmnorris7 <39621216+Cmnorris7@users.noreply.github.com>
2021-12-02 15:44:40 -0500

    chimerism.R from cmd
    chimerism.R can now be run from cmd line by passing file paths & workflow to Rscript.exe

--- R/database.R
+++ R/database.R
@@ -2,12 +2,7 @@
 
 library(DBI)
 
-pre_to_sql <- function(clean_alleles){
-  m <- dbDriver("SQLite")
-  conn <- dbConnect(m,dbname = 'chim_test.db')
-  dbWriteTable(conn, "peak_report", clean_alleles, append = TRUE)
-  dbDisconnect(conn)
-}
+
 
 # post_to_sql <- function(analysis_table){
 #   m <- dbDriver("SQLite")
